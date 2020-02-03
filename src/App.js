3ebd9ca48f3974742dafa26df5bae898c9b7b4e8
codeandsmile1 <codeandsmile@gmail.com>
2020-02-03 20:11:42 +0200

    ..

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import Contact from './components/Contact'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">   
+        <h1>App Component</h1>
+        <Contact/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Contact.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+             <h3>Lybima</h3>
+             <ul>
+                 <li>
+                     Email: lyubima.takova@gmail.com
+                 </li>
+             </ul>
+            </div>
+        )
+    }
+}
+
+export default Contact;
