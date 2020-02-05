9b9eaa5c4c6e8f8e4dcb15319c806e6c82e0681c
codeandsmile1 <codeandsmile@gmail.com>
2020-02-05 15:39:43 +0200

    Added icon react package and function to show/hide the info of the contacts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,27 +1,47 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import './contact.css'
+import "./contact.css";
+import { IoIosArrowDown } from "react-icons/io";
 
 class Contact extends Component {
- 
- //const {name, email, phone} = this.props;
-    render() {
+  constructor() {
+    super();
+    this.onShowClick = this.onShowClick.bind(this);
+    this.state = {
+      showContactInfo: false
+    };
+  }
+
+  onShowClick() {
+    console.log(this);
+    this.setState({ showContactInfo: !this.state.showContactInfo });
+  }
+
+  //const {name, email, phone} = this.props;
+  render() {
     return (
-      <div className="card card-body mb3">
-        <h3>{this.props.name}</h3>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {this.props.email}</li>
-          <li className="list-group-item">Phone: {this.props.phone}</li>
-        </ul>
+      <div className="card card-body mb-3">
+        <h3>
+          {this.props.name}
+          <IoIosArrowDown onClick={this.onShowClick} />
+        </h3>
+        {this.state.showContactInfo == true ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {this.props.email}</li>
+            <li className="list-group-item">Phone: {this.props.phone}</li>
+          </ul>
+        ) : (
+          ""
+        )}
       </div>
     );
   }
 }
 
 Contact.propTypes = {
-   name: PropTypes.string.isRequired,
-   email: PropTypes.string.isRequired,
-   phone: PropTypes.string.isRequired
-}
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired
+};
 
 export default Contact;
