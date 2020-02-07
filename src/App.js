1611d758a57a1c2edabc5f7a0f46aa9dbe38944c
codeandsmile1 <codeandsmile@gmail.com>
2020-02-07 22:19:52 +0200

    Added router..

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Contacts from "./components/contacts/Contacts";
 import Header from "./components/layout/Header";
 import AddContact from "./components/contacts/AddContact"
 import {Provider} from './context'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import About from "./components/pages/About"
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "./App.css";
@@ -10,13 +12,18 @@ import "./App.css";
 function App() {
   return (
     <Provider>
+      <Router>
     <div className="App">   
+    <Header branding="Contact Manager" />
       <div className="container">    
-      <Header branding="Contact Manager" />
-      <AddContact/>
-       <Contacts/> 
+        <Switch>
+          <Route exact path="/" component={Contacts}/>
+          <Route exact path="/contact/add" component={AddContact}/>
+          <Route exact path="/about" component={About}/>
+        </Switch>
       </div>
     </div>
+    </Router>
     </Provider>
   );
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+          <h1 className="display-4">About</h1>
+        </div>
+    )
+}
